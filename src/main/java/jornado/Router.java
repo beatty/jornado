b57ba9b6c2b885ce02b8c18833a5c05a47f2f689
John Beatty <john@9d.local>
2010-05-18 17:40:50 -0700

    oops, tried adding type params, which guice doesn't like.

--- src/main/java/jornado/Router.java
+++ src/main/java/jornado/Router.java
@@ -13,7 +13,7 @@ public class Router<R extends Request> {
 
     @Inject
     @SuppressWarnings("unchecked")
-    public Router(@Named("routes") Iterable<RouteHandler<R>> routeHandlers) {
+    public Router(@Named("routes") Iterable routeHandlers) {
         this.routeHandlers = routeHandlers;
     }
 
