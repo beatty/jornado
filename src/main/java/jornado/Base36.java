40bfee458e3d668bdd1afacf4e4652d781d02960
John Beatty <john.beatty@gmail.com>
2010-08-19 10:41:30 -0700

    base conversion utils

--- src/main/java/jornado/Base36.java
+++ src/main/java/jornado/Base36.java
@@ -4,18 +4,13 @@ package jornado;
  * @author john
  */
 public class Base36 {
-	public static int radix = 36;
+	private static final int RADIX = 36;
 
-	public static String encode(long longValue){
-		return Long.toString(longValue, radix);
+	public static String encode(int value) {
+		return Integer.toString(value, RADIX);
 	}
 
-	public static long decode(String strValue){
-		return Long.parseLong(strValue, radix);
-	}
-
-	public static void main(String[] args) {
-		System.out.println(Base36.encode(1234l));
-		System.out.println(Base36.decode(Base36.encode(1234l)));
+	public static long decode(String base36String){
+		return Integer.parseInt(base36String, RADIX);
 	}
 }
---
+++ src/main/java/jornado/Base62.java
@@ -0,0 +1,46 @@
+package jornado;
+
+import com.google.common.base.Preconditions;
+
+/**
+ * Converts non-negative ints to/from base 62 strings.
+ */
+public class Base62 {
+  private static final char[] CHARS = new char[]{'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
+
+  public static String encode(int value) {
+    Preconditions.checkArgument(value >= 0, "value must be non-negative");
+    String tempVal = "";
+    int mod;
+
+    do {
+      mod = value % 62;
+      tempVal = CHARS[mod] + tempVal;
+      value = value / 62;
+    } while (value != 0);
+
+    return tempVal;
+  }
+
+  public static int decode(String base62String) {
+    char[] chars = base62String.toCharArray();
+    int value = 0;
+    int multiplier = 1;
+    int i = chars.length;
+
+    while (i > 0) {
+      char c = chars[--i];
+      value = value + (indexOf(c) * multiplier);
+      multiplier = multiplier * 62;
+    }
+    return value;
+  }
+
+  // not the fastest, but it's simple.
+  private static int indexOf(char c) {
+    for (int i=0; i<CHARS.length; i++) {
+      if (CHARS[i] == c) return i;
+    }
+    throw new IllegalArgumentException("not a valid char: " + c);
+  }
+}
